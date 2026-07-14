2bb78ccfcf1873309033a6adbb7a72c8bbf71332
ivanss-git <ivan.ibarracs@gmail.com>
2026-07-13 19:15:22 -0500

    docs: completed implementation for 'RemoveNthNode' neetcode submission

---
+++ src/LinkedLists/RemoveNthNodeFromEnd.java
@@ -0,0 +1,48 @@
+package LinkedLists;
+
+public class RemoveNthNodeFromEnd {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val;}
+        ListNode(int val, ListNode next) { this.next = next;}
+
+    }
+
+    public ListNode removeNthNode(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        RemoveNthNodeFromEnd rnn = new RemoveNthNodeFromEnd();
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(2);
+        list.next.next = new ListNode(3);
+        list.next.next.next = new ListNode(4);
+
+        int n = 2;
+        rnn.removeNthNode(list, n);
+        System.out.print("Output: ");
+        ListNode curr = list;
+        while (curr != null) {
+            System.out.print(curr.val + (curr.next != null ? " -> " : ""));
+            curr = curr.next;
+        }
+
+    }
+}
