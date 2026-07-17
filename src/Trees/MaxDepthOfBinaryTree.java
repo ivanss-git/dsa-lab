b3c5e64ee12a4c6529389f1b4275b3e740ffcd85
ivanss-git <ivan.ibarracs@gmail.com>
2026-07-17 12:50:34 -0500

    docs: completed implementation for 'MaxDepthOfBinaryTree'

---
+++ src/Trees/MaxDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+package Trees;
+
+public class MaxDepthOfBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) { return 0;}
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);        
+
+        MaxDepthOfBinaryTree mdobt = new MaxDepthOfBinaryTree();
+        
+        int depth = mdobt.maxDepth(root);
+        System.out.print(depth);
+
+    }
+}
